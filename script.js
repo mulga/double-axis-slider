078c25112d44578572638e31f6990c98b717fdd8
Mehmet Ulga <mehmetulga@gmail.com>
2022-06-14 09:28:36 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,63 @@
+let crouselInner = document.getElementById("crouselInner");
+let slides = document.querySelectorAll(".slide");
+let prevBtn = document.getElementById("prevBtn");
+let nextBtn = document.getElementById("nextBtn");
+let upBtn = document.getElementById("upBtn");
+let downBtn = document.getElementById("downBtn");
+
+let index = 0;
+
+// horizontal scrolling
+
+function horizontal() {
+  if (index > slides.length - 1) {
+    index = 0;
+  } else if (index < 0) {
+    index = slides.length - 1;
+  }
+
+  crouselInner.style.transform = `translateX(${-index * 1440}px)`;
+}
+
+console.log(slides);
+
+prevBtn.addEventListener("click", () => {
+  index--;
+  horizontal();
+});
+
+nextBtn.addEventListener("click", () => {
+  index++;
+  horizontal();
+});
+
+// vertical scrolling
+
+function vertical() {
+  if (index > slides.length - 1) {
+    index = 0;
+  } else if (index < 0) {
+    index = slides.length - 1;
+  }
+  crouselInner.style.flexDirection = `column`;
+  crouselInner.style.transform = `translateY(${-index * 700}px)`;
+}
+
+console.log(slides);
+
+upBtn.addEventListener("click", () => {
+  index++;
+  vertical();
+});
+
+downBtn.addEventListener("click", () => {
+  index--;
+  vertical();
+});
+
+let interval = setInterval(automatic, 3000);
+
+function automatic() {
+  index++;
+  horizontal();
+}
